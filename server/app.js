d2e7ad1b28691186c7270095c861da680a8cfbfa
Riguidix Rodriguez <reydi1@hotmail.com>
2024-03-31 16:45:44 -0600

    Adding DotEnv and Adding flag of server init

--- server/app.js
+++ server/app.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', require('./routes'));
 
 module.exports = app;
