e9fa4d464b621cbe654e6e8e9af4473622434df0
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-03 19:35:30 -0600

    Adding routes for Sign In and Up of Users

--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
+
 import HomePage from './pages/HomePage';
+import SignPage from './pages/SignPage';
 
 export default function AppRouter() {
 	return (
@@ -9,6 +11,8 @@ export default function AppRouter() {
 			<Route element={ <MainLayout /> }>
 				<Route path="/" element={ <HomePage page="home" /> } />
 			</Route>
+			<Route path="/signIn" element={ <SignPage page="signIn" /> } />
+			<Route path="/signUp" element={ <SignPage page="signUp" /> } />
 		</Routes>
 	);
 }
