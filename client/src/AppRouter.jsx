fc0d86d398be63dffd09c3f007d2a975aea1bfe0
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-05 00:08:25 -0600

    Adding register page

--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -4,15 +4,17 @@ import MainLayout from './layouts/MainLayout';
 
 import HomePage from './pages/HomePage';
 import SignPage from './pages/SignPage';
+import RegisterPage from './pages/RegisterPage';
 
 export default function AppRouter() {
 	return (
 		<Routes>
 			<Route element={ <MainLayout /> }>
-				<Route path="/" element={ <HomePage page="home" /> } />
+				<Route path='/' element={ <HomePage page='home' /> } />
+				<Route path='/signUp/register' element={ <RegisterPage page='register' /> } />
 			</Route>
-			<Route path="/signIn" element={ <SignPage page="signIn" /> } />
-			<Route path="/signUp" element={ <SignPage page="signUp" /> } />
+			<Route path='/signIn' element={ <SignPage page='signIn' /> } />
+			<Route path='/signUp' element={ <SignPage page='signUp' /> } />
 		</Routes>
 	);
 }
