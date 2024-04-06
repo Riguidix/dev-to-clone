ebe70db46aad817f92c4d39cfd56b25585cc9dc8
Riguidix Rodriguez <reydi1@hotmail.com>
2024-04-06 15:19:46 -0600

    Adding responsive layout of Sign Page and Login component

--- client/src/components/layout/Login.jsx
+++ client/src/components/layout/Login.jsx
@@ -1,18 +1,13 @@
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import axios from 'axios';
 
 import Button from "../Button";
 
-export default function Login({ handleToggle }) {
+export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const navigate = useNavigate();
-
-    function handleLoginButton () {
+    function handleLogin () {
         if (email !== '' && password !== '') {
             let user = {
                 username: email,
@@ -23,56 +18,57 @@ export default function Login({ handleToggle }) {
                 .then(response => {
                     if (response.data.token && response.data.success) {
                         localStorage.setItem('token', response.data.token);
-                        localStorage.setItem('user_id', response.data);
-                        navigate('/');
+
+                        axios.get(`http://localhost:3000/api/users/${ response.data.data }`)
+                            .then(response => {
+                                let user = {
+                                    email: response.data.data.email,
+                                    username: response.data.data.username,
+                                    id: response.data.data._id,
+                                    profilePicture: response.data.data.profilePicture
+                                }
+                                
+                                localStorage.setItem('user', JSON.stringify(user));
+                            })
+                            .catch(error => console.error('find user:', error));
                     }
                 })
-                .catch(error => console.error(error));
+                .catch(error => console.error('login user:', error));
         }
     }
 
     return (
-        <div className='absolute bg-slate-200 border h-2/3 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-xl rounded-lg p-5 w-2/3'>
-            <div className='grid grid-rows-12 gap-2 h-full p-5 w-full'>
-                <div className='flex items-center justify-end row-span-2'>
-                    <Button
-                        style='ease-in-out flex h-full hover:bg-gray-300 items-center justify-center p-5 rounded-full text-black transition w-auto'
-                        title={ <FontAwesomeIcon icon={ faX } /> }
-                        action={ handleToggle }
-                    />
-                </div>
-
-                <div className='grid grid-rows-2 p-5 row-span-4'>
-                    <label className='text-center text-xl' htmlFor='email_input'>Email</label>
-
+        <div className='flex items-center flex-col justify-start w-full'>
+            <div className='mb-5 w-full'>
+                <div className='w-full'>
+                    <label htmlFor='email_input'>Email</label>
                     <input
                         id='email_input'
-                        className='h-full p-5 rounded w-full'
+                        className='bg-white border border-slate-300 focus:border-sky-400 mt-1 focus:outline-none px-3 py-2 focus:ring-1 focus:ring-sky-500 rounded-md shadow-sm text-md w-full'
                         value={ email }
                         onChange={ (event) => setEmail(event.target.value) }
                         type='text'
                     />
                 </div>
 
-                <div className='grid grid-rows-2 p-5 row-span-4'>
-                    <label className='text-center text-xl' htmlFor='email_input'>Password</label>
-
+                <div className='w-full'>
+                    <label htmlFor='password_input'>Password</label>
                     <input
-                        id='email_input'
-                        className='h-full p-5 rounded w-full'
+                        id='password_input'
+                        className='bg-white border border-slate-300 focus:border-sky-400 mt-1 focus:outline-none px-3 py-2 focus:ring-1 focus:ring-sky-500 rounded-md shadow-sm text-md w-full'
                         value={ password }
                         onChange={ (event) => setPassword(event.target.value) }
                         type='password'
                     />
                 </div>
+            </div>
 
-                <div className='flex items-center justify-center row-span-2'>
-                    <Button
-                        style='bg-gray-400 ease-in-out h-full hover:bg-gray-700 hover:w-3/4 transition-all rounded-xl text-white w-2/3'
-                        title='Log In'
-                        action={ handleLoginButton }
-                    />
-                </div>
+            <div className='w-full'>
+                <Button
+                    style='bg-blue-700 hover:bg-blue-800 ease-in-out px-5 py-2 rounded text-white transition w-full'
+                    title='Log In'
+                    action={ handleLogin }
+                />
             </div>
         </div>
     );
--- client/src/pages/SignPage.jsx
+++ client/src/pages/SignPage.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 import ReactLogo from '../assets/react.svg';
+import Button from '../components/Button';
 import Login from '../components/layout/Login';
 
 export default function SignInPage({ page }) {
-    const [toggle, setToggle] = useState(false);
     const navigate = useNavigate();
 
-    let linksMethods = [
+    let loginMethods = [
         {
             title: 'github',
             icon: faGithub,
@@ -28,72 +27,113 @@ export default function SignInPage({ page }) {
             title: 'email',
             icon: faEnvelope,
             textUp: 'Sign up with Email',
-            textIn: 'Continue with Email',
-        }
+        },
     ];
 
     return (
-        <div className='grid grid-rows-12 gap-1 h-2/3 p-5 relative rounded-xl w-1/3'>
-            {
-                toggle && <Login handleToggle={ () => setToggle(!toggle) } />
-            }
-
-            <div className='row-span-2 flex h-2/3 items-center justify-center w-auto'>
-                <img className='h-full w-auto' src={ ReactLogo } alt='Logo of React' />
+        <div className='flex flex-wrap h-5/6 items-center justify-center w-5/6 md:w-2/3 lg:w-1/3'>
+            <div className='flex items-center justify-center w-full'>
+                <img
+                    className='bg-black hover:cursor-pointer h-1/3 px-3 py-2 rounded-lg w-auto'
+                    src={ ReactLogo }
+                    alt='Logo of React'
+                    onClick={ () => navigate('/') }
+                />
             </div>
 
-            <div className='gap-2 grid grid-rows-2 h-auto text-center py-2 row-span-2 w-full'>
-                <h1 className='font-bold text-4xl'>Join the DEV Community</h1>
-                <h5>DEV Community is a community of 2 amazing developers.</h5>
+            <div className='grid grid-rows-2 text-center w-full'>
+                <h1 className='font-bold text-2xl'>
+                    Join the DEV Community
+                </h1>
+                <h5>DEV Community is a community of 1 amazing developers.</h5>
             </div>
 
-            <div className='gap-2 grid grid-rows-3 mx-auto row-span-5 w-3/4'>
+            <div className='flex flex-col h-2/4 items-center justify-center w-4/5'>
                 {
-                    linksMethods.map((link) => {
-                        return(
-                            <div
-                                className='border gap-2 grid grid-cols-10 hover:cursor-pointer hover:bg-gray-100 hover:text-gray-800 hover:font-bold select-none rounded-xl shadow'
-                                key={ link.title }
-                                onClick={ () => {
-                                    if (page === 'signUp') {
-                                        navigate('/signUp/register');
-                                    } else {
-                                        setToggle(!toggle);
-                                    }
-                                } }
-                            >
-                                <div className='col-span-2 flex items-center justify-center'>
-                                    <FontAwesomeIcon className='h-1/3 w-1/3' icon={ link.icon } />
-                                </div>
+                    loginMethods.map((method, index) => {
+                        return (
+                            (page === 'signIn') 
+                            ?
+                                (method.title === 'email') 
+                                ?
+                                    <>
+                                        <>
+                                            <div className="flex py-5 items-center w-full">
+                                                <div className="flex-grow border-t border-gray-400"></div>
+                                                <span className="flex-shrink mx-4">OR</span>
+                                                <div className="flex-grow border-t border-gray-400"></div>
+                                            </div>
+                                        </>
 
-                                <div className='col-span-8 flex items-center justify-center'>
-                                    { page === 'signUp' ? link.textUp : link.textIn }
+                                        <Login />
+                                    </>
+                                :
+                                <div
+                                    key={ index }
+                                    className='border hover:bg-gray-100 flex h-1/5 items-center justify-center my-2 rounded-md text-center w-full'
+                                >
+                                    <Button
+                                        style='grid grid-cols-4 h-full items-center justify-center w-full'
+                                        title={
+                                            <>
+                                                <FontAwesomeIcon className='text-center w-full' icon={ method.icon } />
+                                                <p className='col-span-3'>
+                                                    { method.textIn }
+                                                </p>
+                                            </>
+                                        }
+                                        action={ () => navigate('/register') }
+
+                                    />
                                 </div>
-                            </div>
+                            :
+                                <div
+                                    key={ index }
+                                    className='border hover:bg-gray-100 flex h-1/5 items-center justify-center my-2 rounded-md text-center w-full'
+                                >
+                                    <Button
+                                        style='grid grid-cols-4 h-full items-center justify-center w-full'
+                                        title={
+                                            <>
+                                                <FontAwesomeIcon className='text-center w-full' icon={ method.icon } />
+                                                <p className='col-span-3'>
+                                                    { method.textUp }
+                                                </p>
+                                            </>
+                                        }
+                                        action={ () => navigate('/register') }
+
+                                    />
+                                </div>
                         );
                     })
                 }
             </div>
 
-            <div className="grid grid-rows-3 row-span-3 mt-5 w-full">
-                <div className='col-span-1 m-auto w-2/3'>
+            <div className='flex flex-wrap items-center justify-center w-full'>
+                <div className='w-full'>
                     <p className='text-sm text-center'>
-                        By signing up, you are agreeing to our <Link className='text-blue-500' to="#">privacy policy</Link>, <Link className='text-blue-500' to="#">terms of user</Link> and <Link className='text-blue-500' to="#">code of conduct</Link>.
+                        By signing up, you are agreeing to our <Link className='text-blue-500' to='#'>privacy policy</Link>, <Link className='text-blue-500' to='#'>terms of user</Link> and <Link className='text-blue-500' to='#'>code of conduct</Link>.
                     </p>
-
-                    <hr className='my-5' />
-
-                    {
-                        (page === 'signUp') ?
-                            <>
-                                <h5 className='text-center'>Already have an account? <Link className='text-blue-500' to="/signIn">Log in</Link>.</h5>
-                            </>
-                        : 
-                            <>
-                                <h5 className='text-center'>New to DEV Community? <Link className='text-blue-500' to="/signUp">Create account</Link>.</h5>
-                            </>
-                    }
                 </div>
+
+                <hr className='my-5 w-full' />
+
+                {
+                    (page === 'signUp')
+                    ?
+                        <>
+                            <h5>
+                                Already have an account? <Link className='text-blue-500' to='/signIn'>Log in</Link>.
+                            </h5>
+                        </>
+                    :
+                        <>
+                            <h5>
+                                New to DEV Community?  <Link className='text-blue-500' to='/signUp'>Create account</Link>.
+                            </h5>
+                        </>
+                }
             </div>
         </div>
     );
