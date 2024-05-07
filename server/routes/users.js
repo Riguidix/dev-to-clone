7f245af686ad94a029ffbaa04baf6a961f7877a3
Riguidix Rodriguez <reydi1@hotmail.com>
2024-05-07 14:53:44 -0600

    Installing Mongoose Unique Validator

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,7 +5,7 @@ const tokenValidator = require("../utils/tokenValidator");
 const userController = require("../controllers/user");
 
 router.post("/login", userController.login);
-router.post("/", tokenValidator, userController.createUser);
+router.post("/", userController.createUser);
 router.get("/", tokenValidator, userController.readUsers);
 router.get("/:id", tokenValidator, userController.readUserById);
 router.put("/:id", tokenValidator, userController.updateUser);
